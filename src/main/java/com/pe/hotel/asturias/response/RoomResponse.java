8fe4d0c60911c7c193b1e30bbf07f4adb4229415
BrayanTorres <brayantorresacuna2023@gmail.com>
2024-07-06 15:06:57 -0500

    Agregar campo description 3

--- src/main/java/com/pe/hotel/asturias/response/RoomResponse.java
+++ src/main/java/com/pe/hotel/asturias/response/RoomResponse.java
@@ -34,8 +34,8 @@ public class RoomResponse {
         this.id = id;
         this.roomType = roomType;
         this.roomPrice = roomPrice;
-        this.isBooked = isBooked;
         this.roomDescription = roomDescription;
+        this.isBooked = isBooked;
         this.photo = photoBytes != null ? Base64.encodeBase64String(photoBytes) : null;
        this.bookings = bookings;
     }
